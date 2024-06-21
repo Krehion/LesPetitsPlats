5bbf63e52cf2106736cf0e6f7d1c9f29e09cb15a
Krehion <krehion@hotmail.com>
2024-06-21 14:53:50 +0200

    [Style] recipe cards layout

--- scripts/templates/recipeCard.js
+++ scripts/templates/recipeCard.js
@@ -1,16 +1,19 @@
 function recipeCardTemplate(data) {
 	const { image, name, ingredients, time, description } = data;
 
+	const recipeImg = `assets/photos/${image}`;
+
 	function getRecipeCardDOM() {
 		// create all needed elements
 		const article = document.createElement("article");
+		article.setAttribute("class", "recipes--card");
 		const imgContainer = document.createElement("div");
-		imgContainer.setAttribute("class", "img-container");
+		imgContainer.setAttribute("class", "recipes--card--img-container");
 		const img = document.createElement("img");
-		img.setAttribute("src", image);
+		img.setAttribute("src", recipeImg);
 		img.setAttribute("alt", name);
 		const txtContainer = document.createElement("div");
-		txtContainer.setAttribute("class", "txt-container");
+		txtContainer.setAttribute("class", "recipes--card--txt-container");
 		const cardTitle = document.createElement("h2");
 		cardTitle.textContent = name;
 		const recipeDescriptionTitle = document.createElement("h3");
@@ -23,10 +26,12 @@ function recipeCardTemplate(data) {
 		ingredientsGrid.setAttribute("class", "ingredients-grid");
 		ingredients.forEach((item) => {
 			const ingredientContainer = document.createElement("div");
-			ingredientContainer.setAttribute("class", "ingredient-container");
+			ingredientContainer.setAttribute("class", "ingredients-grid--block");
 			const ingredientName = document.createElement("p");
 			ingredientName.textContent = item.ingredient;
+			ingredientName.setAttribute("class", "ingredients-grid--name");
 			const ingredientDetail = document.createElement("p");
+			ingredientDetail.setAttribute("class", "ingredients-grid--detail");
 			ingredientDetail.textContent = `${item.quantity || ""} ${item.unit || ""}`.trim();
 			// append elements here : need to be in the forEach
 			ingredientContainer.appendChild(ingredientName);
@@ -34,7 +39,7 @@ function recipeCardTemplate(data) {
 			ingredientsGrid.appendChild(ingredientContainer);
 		});
 		const timeContainer = document.createElement("div");
-		timeContainer.setAttribute("class", "time-container");
+		timeContainer.setAttribute("class", "recipes--card--time-container");
 		const timeContent = document.createElement("p");
 		timeContent.textContent = time + " min";
 
