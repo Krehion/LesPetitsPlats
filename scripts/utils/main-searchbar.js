176243c47484ce3fda93c83816fd84f05cd837a1
Krehion <krehion@hotmail.com>
2024-06-27 11:54:33 +0200

    [Fix] hide cancel button on click

--- scripts/utils/main-searchbar.js
+++ scripts/utils/main-searchbar.js
@@ -1,11 +1,18 @@
 function displayCancelSearch() {
 	const searchInput = document.querySelector(".search-input");
 	const cancelContainer = document.querySelector(".search--cancel--container");
+	const cancelInput = document.querySelector(".search--cancel");
 
 	searchInput.addEventListener("input", () => {
 		if (searchInput.value.length > 0) {
 			cancelContainer.style.display = "inline-block";
 		} else {
+			cancelContainer.style.display = "none";
+		}
+	});
+
+	cancelInput.addEventListener("click", () => {
+		if (cancelContainer.style.display === "inline-block") {
 			cancelContainer.style.display = "none";
 		}
 	});
