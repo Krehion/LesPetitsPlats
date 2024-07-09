1e52bcd4a6b8658b3d4cf054e5cfc17f31a3035d
Krehion <krehion@hotmail.com>
2024-07-09 11:19:24 +0200

    [Refactor] rename files and functions

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,49 +1,55 @@
 async function getRecipes() {
-	const response = await fetch("../../data/recipes.js");
-	if (!response.ok) {
-		console.log("Failed to fetch recipes data");
-		return [];
-	}
-	const scriptText = await response.text(); // Get the script content as text
-	const recipesFunction = new Function(scriptText + "; return recipes;"); // Create a function to evaluate the script
-	const recipes = recipesFunction(); // Execute the function to get the recipes
-	return recipes;
+  const response = await fetch("../../data/recipes.js");
+  if (!response.ok) {
+    console.log("Failed to fetch recipes data");
+    return [];
+  }
+  const scriptText = await response.text(); // Get the script content as text
+  const recipesFunction = new Function(scriptText + "; return recipes;"); // Create a function to evaluate the script
+  const recipes = recipesFunction(); // Execute the function to get the recipes
+  return recipes;
 }
 
 async function displayRecipes(recipes) {
-	const recipeCardsSection = document.querySelector(".recipes");
+  const recipeCardsSection = document.querySelector(".recipes");
 
-	recipes.forEach((recipe) => {
-		const recipeCardModel = recipeCardTemplate(recipe);
-		const recipeCardDOM = recipeCardModel.getRecipeCardDOM();
-		recipeCardsSection.appendChild(recipeCardDOM);
-	});
+  recipes.forEach((recipe) => {
+    const recipeCardModel = recipeCardTemplate(recipe);
+    const recipeCardDOM = recipeCardModel.getRecipeCardDOM();
+    recipeCardsSection.appendChild(recipeCardDOM);
+  });
 }
 
 async function init() {
-	// fetch recipes' data
-	const recipes = await getRecipes(); // Get the recipes array
-	// access the recipes array
-	displayRecipes(recipes);
-	recipeCounter();
+  // fetch recipes' data
+  const recipes = await getRecipes(); // Get the recipes array
+  // access the recipes array
+  displayRecipes(recipes);
+  recipeCounter();
 
-	// fetch keywords arrays
-	const ingredientsKeywords = await getIngredients(recipes);
-	const ingredientsKeywordsSection = document.querySelector(".dropdown-ingredients--keywords");
-	const ustensilesKeywords = await getUstensiles(recipes);
-	const ustensilesKeywordsSection = document.querySelector(".dropdown-ustensiles--keywords");
-	const appareilsKeywords = await getAppareils(recipes);
-	const appareilsKeywordsSection = document.querySelector(".dropdown-appareils--keywords");
+  // fetch keywords arrays
+  const ingredientsKeywords = await getIngredients(recipes);
+  const ingredientsKeywordsSection = document.querySelector(
+    ".dropdown-ingredients--keywords"
+  );
+  const ustensilesKeywords = await getUstensiles(recipes);
+  const ustensilesKeywordsSection = document.querySelector(
+    ".dropdown-ustensiles--keywords"
+  );
+  const appareilsKeywords = await getAppareils(recipes);
+  const appareilsKeywordsSection = document.querySelector(
+    ".dropdown-appareils--keywords"
+  );
 
-	displayDropdownKeywords(ingredientsKeywords, ingredientsKeywordsSection);
-	displayDropdownKeywords(ustensilesKeywords, ustensilesKeywordsSection);
-	displayDropdownKeywords(appareilsKeywords, appareilsKeywordsSection);
-	unfoldDropdown();
-	keywordsInputFilter();
-	displayCancelSearchDropdown();
+  displayDropdownKeywords(ingredientsKeywords, ingredientsKeywordsSection);
+  displayDropdownKeywords(ustensilesKeywords, ustensilesKeywordsSection);
+  displayDropdownKeywords(appareilsKeywords, appareilsKeywordsSection);
+  unfoldDropdown();
+  keywordsInputFilter();
+  displayCancelSearchKeywords();
 
-	createLabel();
-	deleteLabel();
+  createLabel();
+  deleteLabel();
 }
 
 init();
---
+++ scripts/utils/keywords-searchbars.js
@@ -0,0 +1,33 @@
+function displayCancelSearchKeywords() {
+  const searchInputs = document.querySelectorAll(".dropdown--search--input");
+
+  searchInputs.forEach((searchInput) => {
+    // Get the parent container for the current search input
+    const parentContainer = searchInput.closest(".dropdown--search");
+
+    // Find the cancel container and cancel input within the same parent container
+    const cancelContainer = parentContainer.querySelector(
+      ".dropdown--search--cancel--container"
+    );
+    const cancelInput = parentContainer.querySelector(
+      ".dropdown--search--cancel"
+    );
+
+    searchInput.addEventListener("input", () => {
+      if (searchInput.value.length > 0) {
+        cancelContainer.style.display = "inline-block";
+      } else {
+        cancelContainer.style.display = "none";
+      }
+    });
+
+    cancelInput.addEventListener("click", () => {
+      cancelContainer.style.display = "none";
+      searchInput.value = "";
+
+      // trigger the input event to reset the keyword visibility
+      const event = new Event("input", { bubbles: true });
+      searchInput.dispatchEvent(event);
+    });
+  });
+}
