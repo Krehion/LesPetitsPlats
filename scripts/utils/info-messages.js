fdc0cd9919ca2453547b9b8f4d99e3833716ad3d
Krehion <krehion@hotmail.com>
2024-07-19 15:03:17 +0200

    [Fix] add user input variable to error message

--- scripts/utils/info-messages.js
+++ scripts/utils/info-messages.js
@@ -1,5 +1,11 @@
 function displayEmptyResultMessage(filteredRecipes) {
   const emptyResultMessage = document.querySelector(".empty-result-message");
+  const messageVariable = document.querySelector(
+    ".empty-result-message--user-input"
+  );
+  const mainSearchInput = document.querySelector(".search-input");
+  const userInput = mainSearchInput.value.toLowerCase();
+  messageVariable.textContent = userInput;
 
   if (filteredRecipes.length === 0) {
     emptyResultMessage.style.display = "block";
