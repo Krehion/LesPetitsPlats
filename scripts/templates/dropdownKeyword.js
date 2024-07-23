fe09694e153a5ec7a957a4ad31ebe5a8e6f539a3
Krehion <krehion@hotmail.com>
2024-07-23 15:25:40 +0200

    [Fix] class names on advanced filters

--- scripts/templates/dropdownKeyword.js
+++ scripts/templates/dropdownKeyword.js
@@ -1,12 +1,13 @@
 function dropdownKeywordTemplate(item) {
-	function getKeywordDOM() {
-		// create the structure
-		const keywordContainer = document.createElement("button");
-		keywordContainer.setAttribute("class", "dropdown-ingredients--keywords--container");
-		keywordContainer.innerHTML = item + `<i class="fa-solid fa-circle-xmark"></i>`;
+  function getKeywordDOM() {
+    // create the structure
+    const keywordContainer = document.createElement("button");
+    keywordContainer.setAttribute("class", "dropdown--keywords--container");
+    keywordContainer.innerHTML =
+      item + `<i class="fa-solid fa-circle-xmark"></i>`;
 
-		return keywordContainer;
-	}
+    return keywordContainer;
+  }
 
-	return { getKeywordDOM };
+  return { getKeywordDOM };
 }
--- scripts/utils/filterLabels.js
+++ scripts/utils/filterLabels.js
@@ -1,68 +1,81 @@
 function createLabel() {
-	const activeFiltersSection = document.querySelector(".active-labels");
+  const activeFiltersSection = document.querySelector(".active-labels");
 
-	// Select all keywords
-	const filterButtons = document.querySelectorAll(".dropdown-ingredients--keywords--container");
+  // Select all keywords
+  const filterButtons = document.querySelectorAll(
+    ".dropdown--keywords--container"
+  );
 
-	// Add click event listener to each keyword
-	filterButtons.forEach((button) => {
-		button.addEventListener("click", function () {
-			const keywordText = button.textContent;
+  // Add click event listener to each keyword
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const keywordText = button.textContent;
 
-			// Check if the keyword is already selected
-			if (button.classList.contains("dropdown-ingredients--keywords--container__selected")) {
-				// Remove the label if the keyword is already selected
-				const activeFilterLabels = document.querySelectorAll(".active-labels--label");
-				activeFilterLabels.forEach((label) => {
-					if (label.textContent === keywordText) {
-						label.remove();
-					}
-				});
+      // Check if the keyword is already selected
+      if (
+        button.classList.contains("dropdown--keywords--container__selected")
+      ) {
+        // Remove the label if the keyword is already selected
+        const activeFilterLabels = document.querySelectorAll(
+          ".active-labels--label"
+        );
+        activeFilterLabels.forEach((label) => {
+          if (label.textContent === keywordText) {
+            label.remove();
+          }
+        });
 
-				// Toggle the classes back to unselected state
-				button.classList.remove("dropdown-ingredients--keywords--container__selected");
-				button.classList.add("dropdown-ingredients--keywords--container");
-			} else {
-				// Create and add the label
-				const activeFilterLabel = activeFilterLabelTemplate(keywordText);
-				const activeFilterLabelDOM = activeFilterLabel.getActiveFilterLabelDOM();
-				activeFiltersSection.appendChild(activeFilterLabelDOM);
+        // Toggle the classes back to unselected state
+        button.classList.remove("dropdown--keywords--container__selected");
+        button.classList.add("dropdown--keywords--container");
+      } else {
+        // Create and add the label
+        const activeFilterLabel = activeFilterLabelTemplate(keywordText);
+        const activeFilterLabelDOM =
+          activeFilterLabel.getActiveFilterLabelDOM();
+        activeFiltersSection.appendChild(activeFilterLabelDOM);
 
-				// Attach delete event to the newly created label
-				activeFilterLabelDOM.addEventListener("click", function () {
-					activeFilterLabelDOM.remove();
-					// Toggle the button classes back to unselected state
-					button.classList.remove("dropdown-ingredients--keywords--container__selected");
-					button.classList.add("dropdown-ingredients--keywords--container");
-				});
+        // Attach delete event to the newly created label
+        activeFilterLabelDOM.addEventListener("click", function () {
+          activeFilterLabelDOM.remove();
+          // Toggle the button classes back to unselected state
+          button.classList.remove("dropdown--keywords--container__selected");
+          button.classList.add("dropdown--keywords--container");
+        });
 
-				// Change keyword class to "selected": prevents adding same label twice
-				button.classList.add("dropdown-ingredients--keywords--container__selected");
-				button.classList.remove("dropdown-ingredients--keywords--container");
-			}
-		});
-	});
+        // Change keyword class to "selected": prevents adding same label twice
+        button.classList.add("dropdown--keywords--container__selected");
+        button.classList.remove("dropdown--keywords--container");
+      }
+    });
+  });
 }
 
 function deleteLabel() {
-	const activeFilterButtons = document.querySelectorAll(".dropdown-ingredients--keywords--container__selected");
+  const activeFilterButtons = document.querySelectorAll(
+    ".dropdown-ingredients--keywords--container__selected"
+  );
 
-	// Delete when clicking on the active keyword in the dropdown
-	activeFilterButtons.forEach((button) => {
-		button.addEventListener("click", function () {
-			const keywordText = button.textContent;
+  // Delete when clicking on the active keyword in the dropdown
+  activeFilterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const keywordText = button.textContent;
 
-			// Remove the label
-			const activeFilterLabels = document.querySelectorAll(".active-labels--label");
-			activeFilterLabels.forEach((label) => {
-				if (label.textContent === keywordText) {
-					label.remove();
-				}
-			});
+      // Remove the label
+      const activeFilterLabels = document.querySelectorAll(
+        ".active-labels--label"
+      );
+      activeFilterLabels.forEach((label) => {
+        if (label.textContent === keywordText) {
+          label.remove();
+        }
+      });
 
-			// Toggle the button classes back to unselected state
-			button.classList.remove("dropdown-ingredients--keywords--container__selected");
-			button.classList.add("dropdown-ingredients--keywords--container");
-		});
-	});
+      // Toggle the button classes back to unselected state
+      button.classList.remove(
+        "dropdown-ingredients--keywords--container__selected"
+      );
+      button.classList.add("dropdown-ingredients--keywords--container");
+    });
+  });
 }
