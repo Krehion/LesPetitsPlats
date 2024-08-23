01ebc3a9eb9e1e6ff38170f171fd0d03c62e4eec
Krehion <krehion@hotmail.com>
2024-08-23 12:01:44 +0200

    [Refactor] reduce redundancy in search function

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -120,189 +120,132 @@ function search(
   ustensilesKeywordsSelected,
   appareilsKeywordsSelected
 ) {
-  // Get search trigger elements
-  const mainSearchButton = document.querySelector(".search--icon");
-  const ingredientSearchButtons = document.querySelectorAll(
-    ".dropdown-ingredients--keywords .dropdown--keywords--container"
-  );
-  const ustensilSearchButtons = document.querySelectorAll(
-    ".dropdown-ustensiles--keywords .dropdown--keywords--container"
-  );
-  const applianceSearchButtons = document.querySelectorAll(
-    ".dropdown-appareils--keywords .dropdown--keywords--container"
-  );
-  const ingredientRemoveButtons = document.querySelectorAll(
-    ".dropdown-ingredients--keywords .dropdown--keywords--container__selected, .active-ingredients .active-labels--label"
-  );
-  const applianceRemoveButtons = document.querySelectorAll(
-    ".dropdown-appareils--keywords .dropdown--keywords--container__selected, .active-appareils .active-labels--label"
-  );
-  const ustensilRemoveButtons = document.querySelectorAll(
-    ".dropdown-ustensiles--keywords .dropdown--keywords--container__selected, .active-ustensiles .active-labels--label"
-  );
-
-  // Add event listeners
-  mainSearchButton.addEventListener("click", (event) => {
+  // Helper function to handle the common event logic for adding search filters
+  function handleSearch(event, selectedKeywords, searchFunction) {
     event.preventDefault();
-    const newRecipes = mainSearch(recipes); // Get newRecipes from mainSearch
+    const searchText = event.target.textContent.trim().toLowerCase();
+    if (!selectedKeywords.includes(searchText)) {
+      selectedKeywords.push(searchText);
+    }
+    const newRecipes = searchFunction(recipes, searchText);
     run(
       newRecipes,
       ingredientsKeywordsSelected,
       ustensilesKeywordsSelected,
       appareilsKeywordsSelected
-    ); // Pass newRecipes to run
-  });
+    );
+  }
 
-  ingredientSearchButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      const ingredientText = event.target.textContent.trim().toLowerCase(); // Normalize to lowercase
-      // add ingredientText to ingredientsKeywordsSelected array
-      if (!ingredientsKeywordsSelected.includes(ingredientText)) {
-        ingredientsKeywordsSelected.push(ingredientText);
+  // Helper function to handle the common event logic for removing filters
+  async function handleRemoveFilter(event, selectedKeywords) {
+    event.preventDefault();
+
+    const buttonElement = event.target.closest("button");
+    const buttonText = buttonElement?.textContent.trim().toLowerCase();
+
+    if (buttonText && buttonText !== "") {
+      // Correctly update the selectedKeywords array by removing the filter
+      const index = selectedKeywords.indexOf(buttonText);
+      if (index > -1) {
+        selectedKeywords.splice(index, 1); // Use splice to modify the array in place
       }
-      const newRecipes = ingredientSearch(recipes, ingredientText);
+
+      const originalRecipes = await getRecipes();
+      const newRecipes = filterByAllCriteria(
+        originalRecipes,
+        ingredientsKeywordsSelected,
+        appareilsKeywordsSelected,
+        ustensilesKeywordsSelected
+      );
+
       run(
         newRecipes,
         ingredientsKeywordsSelected,
         ustensilesKeywordsSelected,
         appareilsKeywordsSelected
       );
-    });
+    } else {
+      console.warn("Keyword text not found or empty.");
+    }
+  }
+
+  // Get search trigger elements
+  const mainSearchButton = document.querySelector(".search--icon");
+  const filterButtonsConfig = [
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-ingredients--keywords .dropdown--keywords--container"
+      ),
+      selectedKeywords: ingredientsKeywordsSelected,
+      searchFunction: ingredientSearch,
+    },
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-ustensiles--keywords .dropdown--keywords--container"
+      ),
+      selectedKeywords: ustensilesKeywordsSelected,
+      searchFunction: ustensilSearch,
+    },
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-appareils--keywords .dropdown--keywords--container"
+      ),
+      selectedKeywords: appareilsKeywordsSelected,
+      searchFunction: applianceSearch,
+    },
+  ];
+  const removeButtonsConfig = [
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-ingredients--keywords .dropdown--keywords--container__selected, .active-ingredients .active-labels--label"
+      ),
+      selectedKeywords: ingredientsKeywordsSelected,
+    },
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-appareils--keywords .dropdown--keywords--container__selected, .active-appareils .active-labels--label"
+      ),
+      selectedKeywords: appareilsKeywordsSelected,
+    },
+    {
+      buttons: document.querySelectorAll(
+        ".dropdown-ustensiles--keywords .dropdown--keywords--container__selected, .active-ustensiles .active-labels--label"
+      ),
+      selectedKeywords: ustensilesKeywordsSelected,
+    },
+  ];
+
+  // Add event listener for main search button
+  mainSearchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // Get newRecipes from mainSearch
+    const newRecipes = mainSearch(recipes);
+    // Pass newRecipes to run
+    run(
+      newRecipes,
+      ingredientsKeywordsSelected,
+      ustensilesKeywordsSelected,
+      appareilsKeywordsSelected
+    );
   });
 
-  ustensilSearchButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      const ustensilText = event.target.textContent.trim().toLowerCase();
-      if (!ustensilesKeywordsSelected.includes(ustensilText)) {
-        ustensilesKeywordsSelected.push(ustensilText);
-      }
-      const newRecipes = ustensilSearch(recipes, ustensilText);
-      run(
-        newRecipes,
-        ingredientsKeywordsSelected,
-        ustensilesKeywordsSelected,
-        appareilsKeywordsSelected
+  // Add event listeners for each type of filter button
+  filterButtonsConfig.forEach(
+    ({ buttons, selectedKeywords, searchFunction }) => {
+      buttons.forEach((button) => {
+        button.addEventListener("click", (event) =>
+          handleSearch(event, selectedKeywords, searchFunction)
+        );
+      });
+    }
+  );
+
+  // Add event listeners for remove buttons
+  removeButtonsConfig.forEach(({ buttons, selectedKeywords }) => {
+    buttons.forEach((button) => {
+      button.addEventListener("click", (event) =>
+        handleRemoveFilter(event, selectedKeywords)
       );
-    });
-  });
-
-  applianceSearchButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      const applianceText = event.target.textContent.trim().toLowerCase();
-      if (!appareilsKeywordsSelected.includes(applianceText)) {
-        appareilsKeywordsSelected.push(applianceText);
-      }
-      const newRecipes = applianceSearch(recipes, applianceText);
-      run(
-        newRecipes,
-        ingredientsKeywordsSelected,
-        ustensilesKeywordsSelected,
-        appareilsKeywordsSelected
-      );
-    });
-  });
-
-  ingredientRemoveButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      event.preventDefault();
-
-      // Get the text content from the button
-      const buttonElement = event.target.closest("button");
-      const buttonText = buttonElement?.textContent.trim().toLowerCase();
-
-      if (buttonText && buttonText !== "") {
-        ingredientsKeywordsSelected = ingredientsKeywordsSelected.filter(
-          (item) => item !== buttonText
-        );
-
-        const originalRecipes = await getRecipes();
-        console.log("Original Recipes:", originalRecipes);
-        const newRecipes = filterByAllCriteria(
-          originalRecipes,
-          ingredientsKeywordsSelected,
-          appareilsKeywordsSelected,
-          ustensilesKeywordsSelected
-        );
-
-        run(
-          newRecipes,
-          ingredientsKeywordsSelected,
-          ustensilesKeywordsSelected,
-          appareilsKeywordsSelected
-        );
-      } else {
-        console.warn("Ingredient text not found or empty.");
-      }
-    });
-  });
-
-  applianceRemoveButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      event.preventDefault();
-
-      // Get the text content from the button
-      const buttonElement = event.target.closest("button");
-      const buttonText = buttonElement?.textContent.trim().toLowerCase();
-
-      if (buttonText && buttonText !== "") {
-        appareilsKeywordsSelected = appareilsKeywordsSelected.filter(
-          (item) => item !== buttonText
-        );
-
-        const originalRecipes = await getRecipes();
-        console.log("Original Recipes:", originalRecipes);
-        const newRecipes = filterByAllCriteria(
-          originalRecipes,
-          ingredientsKeywordsSelected,
-          appareilsKeywordsSelected,
-          ustensilesKeywordsSelected
-        );
-
-        run(
-          newRecipes,
-          ingredientsKeywordsSelected,
-          ustensilesKeywordsSelected,
-          appareilsKeywordsSelected
-        );
-      } else {
-        console.warn("Ingredient text not found or empty.");
-      }
-    });
-  });
-
-  ustensilRemoveButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      event.preventDefault();
-
-      // Get the text content from the button
-      const buttonElement = event.target.closest("button");
-      const buttonText = buttonElement?.textContent.trim().toLowerCase();
-
-      if (buttonText && buttonText !== "") {
-        ustensilesKeywordsSelected = ustensilesKeywordsSelected.filter(
-          (item) => item !== buttonText
-        );
-
-        const originalRecipes = await getRecipes();
-        const newRecipes = filterByAllCriteria(
-          originalRecipes,
-          ingredientsKeywordsSelected,
-          appareilsKeywordsSelected,
-          ustensilesKeywordsSelected
-        );
-
-        run(
-          newRecipes,
-          ingredientsKeywordsSelected,
-          ustensilesKeywordsSelected,
-          appareilsKeywordsSelected
-        );
-      } else {
-        console.warn("Ingredient text not found or empty.");
-      }
     });
   });
 }
